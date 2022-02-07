d2e565bb9a888999f6c60a0601674bdc9c15f6ef
HelitonWiggers <97917872+HelitonWiggers@users.noreply.github.com>
2022-02-07 17:44:18 -0300

    Add files via upload

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'capybara/rspec'
+require 'site_prism'
+require 'site_prism/all_there'
+require 'rspec'
+require 'rspec/expectations'
+
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome
+end
